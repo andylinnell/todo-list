fd3b77741cde46ecbf907edf597b9fbb6517ff10
Andy Linnell <andylinnell.cafo@gmail.com>
2023-02-15 11:41:47 -0500

    added updateTask and addTask functions

--- functions/src/tasks.js
+++ functions/src/tasks.js
@@ -1,12 +1,31 @@
 import { getFirestoreInstance } from "./utils.js";
+import { FieldValue } from "firebase-admin/firestore";
 
-export function getAllTasks(req,res) {
+export function getAllTasks(req, res) {
     const db = getFirestoreInstance();
-    
-    db.collection('tasks').get()
-    .then(collection => {
-        const tasks = collection.docs.map(doc => ({ taskId: doc.id, ...doc.data() }));
-        res.send(tasks);
-    })
-    .catch(err => res.status(500).json({ error: err.message }));
+
+    db.collection('tasks').sortBy('createdAt', 'desc').get()
+        .then(collection => {
+            const tasks = collection.docs.map(doc => ({ taskId: doc.id, ...doc.data() }));
+            res.send(tasks);
+        })
+        .catch(err => res.status(500).json({ error: err.message }));
+}
+
+export async function addTask(req, res) {
+    const { task } = req.body;
+    const newTask = { task, createdAt: FieldValue.serverTimestamp() }
+    const db = await getFirestoreInstance();
+    db.collection('tasks').add(newTask)
+        .then(() => getAllTasks(req, res)) // instantly sends back change and shows with change, skips a whole step of front end calling for information after change
+        .catch(err => res.status(500).send({ error: err.message }));
+}
+
+export async function updateTask(req, res) {
+    const { done } = req.body;
+    const { taskId } = req.params;
+    const db = await getFirestoreInstance();
+    db.collection('tasks').doc(taskId).update({ done })
+    .then(() => getAllTasks(req, res)) 
+    .catch(err => res.status(500).send({ error: err.message }));
 }
